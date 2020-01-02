348d68ecce61d3551cc297fbb6977610bf8a809b
Sachin <Sachin@HP>
2020-01-02 20:11:24 +0530

    first commit saved all details

---
+++ CalculatorEx/src/com/sachin/Client.java
@@ -0,0 +1,13 @@
+package com.sachin;
+
+public class Client {
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		c.add(20, 12);
+		c.subtract(55, 10);
+		c.multiply(4, 8);
+		c.divide(10, 5);
+	}
+
+
+}
