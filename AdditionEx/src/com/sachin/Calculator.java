004108619abb7780e4196c43819b8e3f6e2042c3
Sachin <Sachin@HP>
2020-01-02 19:49:44 +0530

    first commit uploading All files

---
+++ AdditionEx/src/com/sachin/Calculator.java
@@ -0,0 +1,26 @@
+package com.sachin;
+
+public class Calculator {
+
+	public int add(int num1,int num2) {
+		int result=num1+num2;
+		System.out.println(result);
+		return result;
+	}
+	public int subtract(int num1,int num2) {
+		int result=num1-num2;
+		System.out.println(result);
+		return result;
+	}
+	public int multiply(int num1,int num2) {
+		int result=num1*num2;
+		System.out.println(result);
+		return result;
+	}
+	public int divide(int num1,int num2) {
+		int result=num1/num2;
+		System.out.println(result);
+		return result;
+	}
+	
+}
---
+++ AdditionEx/src/com/sachin/Client.java
@@ -0,0 +1,9 @@
+package com.sachin;
+
+public class Client {
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		c.add(20, 12);
+	}
+
+}
